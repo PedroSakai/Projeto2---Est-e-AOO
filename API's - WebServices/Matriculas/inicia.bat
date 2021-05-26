d30438cac6a6e50627288a8bd071d0c2556fef25
PedroSakai <75867370+PedroSakai@users.noreply.github.com>
2021-05-26 18:55:48 -0300

    Delete API's - WebServices directory

--- API's - WebServices/Matriculas/inicia.bat
+++
@@ -1,4 +0,0 @@
-path C:\Arquivos de Programas\instantclient_19_6;C:\Arquivos de Programas\node-v14.16.1-x64
-set  NODE_ORACLEDB_CONNECTIONSTRING=localhost/xe
-set  NODE_ORACLEDB_USER=SYSTEM
-set  NODE_ORACLEDB_PASSWORD=senha123
